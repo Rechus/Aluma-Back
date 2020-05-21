dfbeb3212cd6d420ec2644f86583fc07b8850f11
Rafa_Reche <rafareche84@gmail.com>
2020-05-21 10:20:23 +0000

    Revert "front cambios previo despliegue"
    This reverts commit 933c12bed8e357a68ba96f8f1f83d6518613e665

--- front/alumaFront/src/app/comun/comun.module.ts
+++ front/alumaFront/src/app/comun/comun.module.ts
@@ -7,7 +7,9 @@ import { LateralComponent } from './lateral/lateral.component';
 import { HomeComponent } from './home/home.component';
 import { PieComponent } from './pie/pie.component';
 import { ServiciosModule} from './../servicios/servicios.module';
-
+import { MatIconModule} from '@angular/material/icon';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
 
 
 
@@ -16,6 +18,9 @@ import { ServiciosModule} from './../servicios/servicios.module';
   imports: [
     CommonModule,
     ServiciosModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
     FormsModule,
     RouterModule
   ],
@@ -24,6 +29,9 @@ import { ServiciosModule} from './../servicios/servicios.module';
     LateralComponent, 
     HomeComponent, 
     PieComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
     FormsModule
   ]
 })
